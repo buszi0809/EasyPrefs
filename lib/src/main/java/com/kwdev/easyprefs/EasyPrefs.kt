581bdad7f9df7ff5e8b32624c9226086d1848a8f
Kacper Wojciechowski <delak08090@gmail.com>
2021-11-15 20:45:18 +0100

    Move delegates to be a part of abstraction

--- lib/src/main/java/com/kwdev/easyprefs/EasyPrefs.kt
+++ lib/src/main/java/com/kwdev/easyprefs/EasyPrefs.kt
@@ -1,6 +1,9 @@
 package com.kwdev.easyprefs
 
 import android.content.SharedPreferences
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.kwdev.easyprefs.properties.*
 import kotlin.reflect.KProperty
 
 /**
@@ -9,5 +12,75 @@ import kotlin.reflect.KProperty
 abstract class EasyPrefs(
     val prefs: SharedPreferences,
 ) {
+    /**
+     * Get EasyPrefs property key for given [KProperty]. The property needs to be declared within the same class.
+     */
     fun getKeyFor(property: KProperty<*>): String = getKeyFor(this, property)
+
+    /**
+     * [Boolean] easy prefs delegate.
+     */
+    protected fun boolean(
+        commit: Boolean = false,
+        default: Boolean = false,
+    ): EasyPrefsProperty<Boolean> = EasyBooleanProperty(commit, default)
+
+    /**
+     * [Int] easy prefs delegate.
+     */
+    protected fun integer(
+        commit: Boolean = false,
+        default: Int = 0,
+    ): EasyPrefsProperty<Int> = EasyIntegerProperty(commit, default)
+
+    /**
+     * [Long] easy prefs delegate.
+     */
+    protected fun long(
+        commit: Boolean = false,
+        default: Long = 0L,
+    ): EasyPrefsProperty<Long> = EasyLongProperty(commit, default)
+
+    /**
+     * [String] easy prefs delegate.
+     */
+    protected fun string(
+        commit: Boolean = false,
+        default: String = "",
+    ): EasyPrefsProperty<String> = EasyStringProperty(commit, default)
+
+    /**
+     * Nullable [String] easy prefs delegate.
+     */
+    protected fun nullableString(
+        commit: Boolean = false,
+        default: String? = null,
+    ): EasyPrefsProperty<String?> = EasyNullableStringProperty(commit, default)
+
+    /**
+     * [Set] of [String] easy prefs delegate.
+     */
+    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
+    protected fun stringSet(
+        commit: Boolean = false,
+        default: Set<String> = setOf(),
+    ): EasyPrefsProperty<Set<String>> = EasyStringSetProperty(commit, default)
+
+    /**
+     * Nullable [Set] of [String] easy prefs delegate.
+     */
+    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
+    protected fun nullableStringSet(
+        commit: Boolean = false,
+        default: Set<String>? = null,
+    ): EasyPrefsProperty<Set<String>?> = EasyNullableStringSetProperty(commit, default)
+
+    /**
+     * Custom type [T] easy prefs delegate. Requires [TypeAdapter] implementation of type [T].
+     */
+    protected fun <T> custom(
+        adapter: TypeAdapter<T>,
+        commit: Boolean = false,
+        default: T? = null,
+    ): EasyPrefsProperty<T> = EasyCustomTypeProperty(commit, adapter, default)
 }
--- lib/src/main/java/com/kwdev/easyprefs/EasyPrefsFlow.kt
+++ lib/src/main/java/com/kwdev/easyprefs/EasyPrefsFlow.kt
@@ -1,10 +1,14 @@
 package com.kwdev.easyprefs
 
 import android.content.SharedPreferences
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.kwdev.easyprefs.flow.*
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filter
+import kotlin.reflect.KProperty
 
 /**
  * Abstraction with basic functionality required to use easy prefs delegates, and extended functionality to use flow delegates.
@@ -31,6 +35,90 @@ abstract class EasyPrefsFlow(
     }
 
     internal fun propertyFlow(key: String): Flow<String> = _keysFlow.filter(key::equals)
+
+    /**
+     * Kotlin flow delegate of [Boolean] easy prefs property.
+     * The [key] is property that you want to observe with use of this flow.
+     * The [key] property needs to be declared within the same class as this flow delegate.
+     */
+    protected fun integerFlow(
+        key: KProperty<Int>,
+        default: Int = 0,
+    ): EasyPropertyFlow<Int> = EasyIntegerPropertyFlow(key, default)
+
+    /**
+     * Kotlin flow delegate of [Long] easy prefs property.
+     * The [key] is property that you want to observe with use of this flow.
+     * The [key] property needs to be declared within the same class as this flow delegate.
+     */
+    protected fun longFlow(
+        key: KProperty<Long>,
+        default: Long = 0L,
+    ): EasyPropertyFlow<Long> = EasyLongPropertyFlow(key, default)
+
+    /**
+     * Kotlin flow delegate of [Boolean] easy prefs property.
+     * The [key] is property that you want to observe with use of this flow.
+     * The [key] property needs to be declared within the same class as this flow delegate.
+     */
+    protected fun booleanFlow(
+        key: KProperty<Boolean>,
+        default: Boolean = false,
+    ): EasyPropertyFlow<Boolean> = EasyBooleanPropertyFlow(key, default)
+
+    /**
+     * Kotlin flow delegate of [Set] of [String] easy prefs property.
+     * The [key] is property that you want to observe with use of this flow.
+     * The [key] property needs to be declared within the same class as this flow delegate.
+     */
+    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
+    protected fun stringSetFlow(
+        key: KProperty<Set<String>>,
+        default: Set<String>,
+    ): EasyPropertyFlow<Set<String>> = EasyStringSetPropertyFlow(key, default)
+
+    /**
+     * Kotlin flow delegate of nullable [Set] of [String] easy prefs property.
+     * The [key] is property that you want to observe with use of this flow.
+     * The [key] property needs to be declared within the same class as this flow delegate.
+     */
+    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
+    protected fun nullableStringSetFlow(
+        key: KProperty<Set<String>?>,
+        default: Set<String>? = null,
+    ): EasyPropertyFlow<Set<String>?> = EasyNullableStringSetPropertyFlow(key, default)
+
+    /**
+     * Kotlin flow delegate of [String] easy prefs property.
+     * The [key] is property that you want to observe with use of this flow.
+     * The [key] property needs to be declared within the same class as this flow delegate.
+     */
+    protected fun stringFlow(
+        key: KProperty<String>,
+        default: String,
+    ): EasyPropertyFlow<String> = EasyStringPropertyFlow(key, default)
+
+    /**
+     * Kotlin flow delegate of nullable [String] easy prefs property.
+     * The [key] is property that you want to observe with use of this flow.
+     * The [key] property needs to be declared within the same class as this flow delegate.
+     */
+    protected fun nullableStringFlow(
+        key: KProperty<String?>,
+        default: String? = null,
+    ): EasyPropertyFlow<String?> = EasyNullableStringPropertyFlow(key, default)
+
+    /**
+     * Kotlin flow delegate of custom type [T] easy prefs property.
+     * The [key] is property that you want to observe with use of this flow.
+     * The [key] property needs to be declared within the same class as this flow delegate.
+     * Requires [TypeAdapter] implementation of type [T].
+     */
+    protected fun <T> customFlow(
+        key: KProperty<T>,
+        adapter: TypeAdapter<T>,
+        default: T? = null,
+    ): EasyPropertyFlow<T> = EasyCustomTypePropertyFlow(key, adapter, default)
 
     companion object {
         const val DEFAULT_BUFFER_CAPACITY = 64
