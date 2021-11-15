82c56121e4c8fb6a0499e8942b8d64a155bd1647
Kacper Wojciechowski <delak08090@gmail.com>
2021-11-15 20:47:44 +0100

    Adjust example test app

--- app/src/main/java/com/kwdev/easyprefs/MainActivity.kt
+++ app/src/main/java/com/kwdev/easyprefs/MainActivity.kt
@@ -2,34 +2,63 @@ package com.kwdev.easyprefs
 
 import android.content.Context
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.kwdev.easyprefs.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val textView = findViewById<TextView>(R.id.textView)
-        val button = findViewById<Button>(R.id.button)
+        setupFlow(binding)
+        setupRx(binding)
+    }
 
-        val preferences = Preferences(getPreferences(Context.MODE_PRIVATE), ByteArrayAdapter())
+    private fun setupFlow(binding: ActivityMainBinding) {
+        val preferences = PreferencesFlow(
+            getSharedPreferences("Preferences flow", Context.MODE_PRIVATE),
+            ByteArrayAdapter(),
+        )
 
-        textView.text = preferences.string
-        var count = preferences.string.substringAfter('+').toIntOrNull() ?: 0
-        button.setOnClickListener {
-            count++
-            preferences.string = "Something new +$count"
-            textView.text = preferences.string
+        preferences.stringFlow
+            .onEach { value ->
+                binding.textViewFlow.text = value
+                Toast.makeText(this, "Data from flow: $value", Toast.LENGTH_SHORT).show()
+            }
+            .launchIn(lifecycleScope)
+
+        binding.buttonFlow.setOnClickListener {
+            val counter = preferences.string.toInt() + 1
+            preferences.string = counter.toString()
         }
+    }
 
-        preferences.stringFlow.onEach {
-            Toast.makeText(this, it.orEmpty(), Toast.LENGTH_LONG).show()
-        }.launchIn(lifecycleScope)
+    private fun setupRx(binding: ActivityMainBinding) {
+        val preferences = PreferencesRx(
+            getSharedPreferences("Preferences rx", Context.MODE_PRIVATE),
+            ByteArrayAdapter(),
+        )
+
+        preferences.stringObservable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy { value ->
+                binding.textViewRx.text = value
+                Toast.makeText(this, "Data from observable: $value", Toast.LENGTH_SHORT).show()
+            }
+
+        binding.buttonRx.setOnClickListener {
+            val counter = preferences.string.toInt() + 1
+            preferences.string = counter.toString()
+        }
     }
 }
--- app/src/main/java/com/kwdev/easyprefs/Preferences.kt
+++ app/src/main/java/com/kwdev/easyprefs/Preferences.kt
@@ -2,13 +2,24 @@ package com.kwdev.easyprefs
 
 import android.content.SharedPreferences
 
-class Preferences(
+class PreferencesFlow(
     sharedPreferences: SharedPreferences,
     byteArrayAdapter: TypeAdapter<ByteArray?>,
 ) : EasyPrefsFlow(sharedPreferences) {
-    var string by string(commit = true, default = "Some value")
+    var string by string(commit = true, default = "0")
     var byteArray by custom(adapter = byteArrayAdapter, commit = true)
 
-    val stringFlow by stringFlow(key = ::string)
+    val stringFlow by stringFlow(key = ::string, default = "0")
     val byteArrayFlow by customFlow(key = ::byteArray, adapter = byteArrayAdapter)
+}
+
+class PreferencesRx(
+    sharedPreferences: SharedPreferences,
+    byteArrayAdapter: TypeAdapter<ByteArray?>,
+) : EasyPrefsRx(sharedPreferences) {
+    var string by string(commit = true, default = "0")
+    var byteArray by custom(adapter = byteArrayAdapter, commit = true)
+
+    val stringObservable by stringObservable(key = ::string, default = "0")
+    val byteArrayObservable by customObservable(key = ::byteArray, adapter = byteArrayAdapter)
 }
